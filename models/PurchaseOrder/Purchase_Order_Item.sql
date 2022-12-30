0fa7ab58ec3e52e686a40b29661d14734738073e
wondewossen@gmail.com <wondewossen@gmail.com>
2022-12-30 14:30:13 +0000

    Field Type changed

--- models/PurchaseOrder/Purchase_Order_Item.sql
+++ models/PurchaseOrder/Purchase_Order_Item.sql
@@ -1,7 +1,6 @@
   {{config( 
-  --        materialized='incremental',
-  --        incremental_strategy='merge',
-  --        unique_key='Order' 
+          materialized='incremental',
+          unique_key='POOrder' 
         )      
     }}    
 
@@ -12,12 +11,10 @@ with Get_PurchaseOrder as (
         left outer join SPARC_RAW.S4_GF.CUSTOMER c
             on p.customer = c.customer
 
- --   {% if is_incremental() %}        
-    --where (p.purchaseorder, p.purchaseorderitem, p.__timestamp) in
-    --        (select purchaseorder,purchaseorderItem, max(__timestamp) as __timestamp
-    --              from SPARC_RAW.S4_GF.PURORDITEM where purchaseorder in ('4500000000','4500000275') group by 1,2)
- --       where __timestamp > (select max(__create_datetime) from {{ this }})
- --   {% endif %}
+    {% if is_incremental() %}        
+        where p.__timestamp < (select max(Createdatetime) from {{ this }})
+    {% endif %}
+
 ),
 
 Get_headerLevel as (
@@ -40,29 +37,29 @@ Get_Manufacturer as (
             on p.supplyingsupplier = s.supplier)
 
 select 
-        purchaseorder       as "Order",
-        customer            as "Customer Number",
-        customername        as "Buyer Name",
-        supplier_name       as "Seller Name",
-        supplier            as "Vendor Code",
-        Manufacturer_name   as "Manufacturer Name",
+        purchaseorder       as POOrder,
+        customer            as CustomerNumber,
+        customername        as BuyerName,
+        supplier_name       as SellerName,
+        supplier            as VendorCode,
+        Manufacturer_name   as ManufacturerName,
         case when deletion_idn is null 
                 then 'Open'
                 else 'Canceled'
-        end                 as "Order Status",
-        podoctype           as "Order Type",
-        shippingconditions  as "PO Priority",
-        crmrefordernumber   as "Customer PO Ref. No",
-        companycode         as "Buyer Code",
-        incoterms1          as "Incoterm",
-        incoterms2          as "Incoterm Location",
-        termsofpaymentkey   as "Payment Term",
-        changeddate         as "Modify Timestamp",
-        ship_type_des       as "Shipment Method",
-        __timestamp         as "create_datetime",
-        count(*)            as "Item Count",
-        sum(orderqty)       as "Order Total Qty",
-        sum(netordervalue)  as "Order Total Amount",
-        currencykey         as "Currency"
+        end                 as OrderStatus,
+        podoctype           as OrderType,
+        shippingconditions  as POPriority,
+        crmrefordernumber   as CustomerPORefNo,
+        companycode         as BuyerCode,
+        incoterms1          as Incoterm,
+        incoterms2          as IncotermLocation,
+        termsofpaymentkey   as PaymentTerm,
+        changeddate         as ModifyTimestamp,
+        ship_type_des       as ShipmentMethod,
+        __timestamp         as Createdatetime,
+        count(*)            as ItemCount,
+        sum(orderqty)       as OrderTotalQty,
+        sum(netordervalue)  as OrderTotalAmount,
+        currencykey         as Currency
  from Get_Manufacturer 
 group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,21
